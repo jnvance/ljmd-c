dbd52537cfe82a8269359b6e4099b03b20cb57e8
James Vance <jvance@ictp.it>
2017-02-10 13:48:26 +0100

    general inputfile

--- python/ljmd.py
+++ python/ljmd.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 from ctypes import *
-import os
-
-mdso = cdll.LoadLibrary("../obj/ljmd.so")
+import os, sys
 
 def run_mdsim(inputfile):
-
+	
 	# parse inputfile
 	f = open(inputfile, 'r')
 	natoms,mass,epsilon,sigma,rcut,box,restfile,\
@@ -27,12 +25,15 @@ def run_mdsim(inputfile):
 	c_ergfile  = c_char_p("".join(ergfile.split()).encode())
 
 	# run simulation
+	mdso = cdll.LoadLibrary("../obj/ljmd.so")
 	mdso.mdsim(c_nprint,c_natoms,c_nsteps,c_mass,c_epsilon,c_sigma,
 			   c_box,c_rcut,c_dt,c_restfile.value,c_trajfile.value,c_ergfile.value)
 
 
 if __name__ == '__main__':
 	
-	
-	inputfile = "argon_108.inp"
-	run_mdsim(inputfile)
+	if len(sys.argv) > 1:
+		inputfile = str(sys.argv[1])
+		run_mdsim(inputfile)
+	else:
+		pass
